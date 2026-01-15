e1cb2fd8a0b1256c0043a90b3be2b3bca63d0ca5
Kebé Mamadou <24079@esp.mr>
2026-01-15 10:59:24 +0100

    TP4: configure PostgreSQL and expose Django server

--- app/config/settings.py
+++ app/config/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "sid_db",
+        "USER": "sid_user",
+        "PASSWORD": "sid_pass",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
 
